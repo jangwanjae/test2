132bf34f0f5e857f6d0b2b0920b4442a1f1ced97
Chosun Geun <eddy0215@kakao.com>
2024-05-20 10:33:15 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import student
+
+# 문제 1. 학생의 이름을 딕셔너리 자료형을 이용하여 
+# 키가 1부터 시작하는 데이터를 아래에 작성하세요. (데이터 10개 입력)
+student_list = {????}
+
+student.stu_list(student_list)
+student.stu_list_head()
+student.stu_list_fomatted(student_list)
---
+++ student.py
@@ -0,0 +1,15 @@
+def stu_list(list):
+    return print(list)
+
+# 문제2. 아래 함수를 작성하여
+# 학번과 이름을 표시하는 머리글을 출력하시오.
+def stu_list_head():
+    print(????)
+
+# 문제3. 아래 함수를 작성하여 딕셔너리로 부터 
+# 학번과 이름을 분리하여 학급명렬표를 출력하시오.
+def stu_list_fomatted(list):
+    fotmatted_list = ""
+    ????
+        ????
+    return print(fotmatted_list)
